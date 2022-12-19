84e0999e61289e9236a3724c800ca977886b14f5
Guillaume Lung Tung <register@talaron.net>
2022-12-20 01:32:56 +0400

    version 41

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ app.mount("/public", StaticFiles(directory="/app/public"), name="public")
 ######################################
 @app.get("/")
 async def read_index():
-    return RedirectResponse(url="/public/index.html?v=40")   
+    return RedirectResponse(url="/public/index.html?v=41")   
 
 ###################
 ## authentification
